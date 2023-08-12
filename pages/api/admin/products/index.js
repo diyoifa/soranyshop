ebe0d6744846dc8ead8e2db763316c3af0fa6936
Gregorio Cardona <dani12ca12@gmail.com>
2023-08-12 09:32:04 -0500

    plzWork

--- pages/api/admin/products/index.js
+++ pages/api/admin/products/index.js
@@ -13,17 +13,17 @@ dbConnect();
 
 // handler.use(isAuthenticatedUser, authorizeRoles("admin")).post(newProduct);
 // Agrega el middleware CORS aquí
-handler.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://soranyshop.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-  } else {
-    next();
-  }
-});
+// handler.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "https://soranyshop.vercel.app");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   if (req.method === "OPTIONS") {
+//     res.status(200).end();
+//   } else {
+//     next();
+//   }
+// });
 handler.post(newProduct);
 
 export default handler;
